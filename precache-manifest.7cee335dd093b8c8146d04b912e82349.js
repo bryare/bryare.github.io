e6e88ec063774e66fa623717187af895fac0f759
b <bareval001@citymail.cuny.edu>
2020-05-29 10:43:09 -0400

    Updates

---
+++ precache-manifest.7cee335dd093b8c8146d04b912e82349.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d356db04e9c45b6cff3670e68c4673ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da6899487e238d2b7ba",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b7d37259b32f5f0d0134",
+    "url": "/static/js/2.77e92f18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77e92f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da6899487e238d2b7ba",
+    "url": "/static/js/main.2c5eb673.chunk.js"
+  },
+  {
+    "revision": "720ad3ebd8901003256b",
+    "url": "/static/js/runtime-main.6f491992.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
